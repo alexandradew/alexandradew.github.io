52ce1a62a6e58a684fd7984ac4bf816351de4708
alexandradew <alexandradew@protonmail.com>
2020-05-13 01:52:57 -0300

    update

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".arrow").click(function(){
-  	$([document.documentElement, document.body]).animate({
-       scrollTop: $("#projects").offset().top
-    }, 1000);
-	});
-});
-
